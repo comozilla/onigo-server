fa0b71c057f8fa586f354c8aa4ad723249123233
nakamurataichi <ringo.m3@icloud.com>
2016-06-11 15:49:46 +0900

    VirtualSpheroを画面からはみ出さないようにした

--- js/virtual-target.js
+++ js/virtual-target.js
@@ -11,17 +11,25 @@ VirtualSphero.prototype.constructor = VirtualSphero;
 VirtualSphero.prototype.setPosition = function(x, y) {
   var degree = Math.atan2(x, y);
   degree = 360 - ((degree / Math.PI * 180) + 180);
-  console.log(degree);
   var far = 10; //TODO farもpositionからとる
   var radian = (degree * Math.PI / 180);
 
   this.x += Math.sin(radian) * far;
   this.y -= Math.cos(radian) * far;
 
+  this.fixPosition();
   this.updateSpheroPosition();
 };
 
 VirtualSphero.prototype.updateSpheroPosition = function() {
   this.element.style.left = this.x + "px";
   this.element.style.top = this.y + "px";
+};
+
+VirtualSphero.prototype.fixPosition = function () {
+  this.x = Math.max(this.x, 0);
+  this.y = Math.max(this.y, 0);
+
+  this.x = Math.min(this.x, window.innerWidth - 50);
+  this.y = Math.min(this.y, window.innerHeight - 50);
 };
