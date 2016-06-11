6760df682d5239730a4de2f7344ca5bbf3ac9881
nakamurataichi <ringo.m3@icloud.com>
2016-06-11 16:54:50 +0900

    VirtualSpheroをcanvasに変更

--- js/virtual-target.js
+++ js/virtual-target.js
@@ -1,8 +1,12 @@
 function VirtualSphero() {
-  this.element = document.getElementById("sphero");
-  var style = getComputedStyle(this.element, "");
-  this.x = parseInt(style.left);
-  this.y = parseInt(style.top);
+  this.canvas = document.getElementById("canvas");
+  this.canvas.width = window.innerWidth;
+  this.canvas.height = window.innerHeight;
+  this.ctx = this.canvas.getContext("2d");
+
+  this.x = 0;
+  this.y = 0;
+  this.radius = 25;
 }
 
 VirtualSphero.prototype = Object.create(TargetBase);
@@ -22,9 +26,15 @@ VirtualSphero.prototype.setPosition = function(x, y) {
 };
 
 VirtualSphero.prototype.updateSpheroPosition = function() {
-  this.element.style.left = this.x + "px";
-  this.element.style.top = this.y + "px";
+  this.clearCanvas();
+  this.ctx.beginPath();
+  this.ctx.arc(this.x + this.radius, this.y + this.radius, this.radius, 0, Math.PI * 2, true);
+  this.ctx.stroke();
 };
+
+VirtualSphero.prototype.clearCanvas = function() {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
 
 VirtualSphero.prototype.fixPosition = function () {
   this.x = Math.max(this.x, 0);
