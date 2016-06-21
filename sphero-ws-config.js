3fba0b442bd7458dc7992e2cdc3eecac91f3b368
shundroid <shundroid@users.noreply.github.com>
2016-06-21 19:56:52 +0900

    change to use sphero-websocket-side virtual-sphero

--- sphero-ws-config.js
+++ sphero-ws-config.js
@@ -1,8 +1,12 @@
+var VirtualPlugin = require("sphero-ws-virtual-plugin");
+
 module.exports = {
     wsPort: 8080,
     allowedOrigin: "*",
     sphero: [
         {name: "Sphero-BPO", port: "COM3"}
+    ],
+    plugins: [
+      new VirtualPlugin()
     ]
 };
-
