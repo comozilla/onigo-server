e6b21b6ef89c6a067baecd4fb3f9ea05629a6998
shundroid <shundroid@users.noreply.github.com>
2016-06-26 15:36:45 +0900

    角度が360以上になるバグを修正

--- js/joystick.js
+++ js/joystick.js
@@ -82,7 +82,7 @@ function getFixedPosition(x, y, maxDistance) {
 
 function toDegreeAndSpeed(x, y, maxDistance) {
   var degree = Math.atan2(y, x);
-  degree = Math.floor((degree / Math.PI * 180 + 450));
+  degree = Math.floor((degree / Math.PI * 180 + 450) % 360);
 
   // getDistanceで取れる値の範囲は、
   // 0～this.maxDistanceである。
