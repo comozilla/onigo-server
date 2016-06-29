729e998e9b467de43460d5e019b760a108e4e5c4
shundroid <shundroid@users.noreply.github.com>
2016-06-29 15:19:16 +0900

    sphero-ws 0.4.2 を使用し、collisionを検知できるように

--- main.js
+++ main.js
@@ -6,14 +6,19 @@ var VirtualSphero = require("sphero-ws-virtual-plugin");
 var opts = [
   { name: "test", type: "boolean" }
 ];
-
 var args = argv.option(opts).run();
 
-var result = spheroWebSocket(config.websocket, args.options.test);
+var spheroWS = spheroWebSocket(config.websocket, args.options.test);
 
 var virtualSphero = new VirtualSphero(config.virtualSphero.wsPort);
-
-
-result.events.on("command", function(requestKey, command, args) {
+spheroWS.events.on("command", function(requestKey, command, args) {
   virtualSphero.command(command, args);
+});
+
+spheroWS.events.on("addClient", function(key, client) {
+  var orb = client.linkedOrb.instance;
+  orb.detectCollisions();
+  orb.on("collision", function() {
+    console.log("hit!");
+  });
 });
