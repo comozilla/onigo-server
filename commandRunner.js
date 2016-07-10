c9c4139d0106449ecfe7aba0d7a1b995eb65c3c9
shundroid <shundroid@users.noreply.github.com>
2016-07-10 14:44:51 +0900

    commandの実行部分を分割

---
+++ commandRunner.js
@@ -0,0 +1,78 @@
+var EventEmitter = require("events").EventEmitter;
+var util = require("util");
+
+function CommandRunner(key) {
+  EventEmitter.call(this);
+
+  this.key = key;
+  this.commands = [];
+  this.baseSpeed = 0;
+  this.angle = 0;
+  this.timeoutId = null;
+  this.dashTimeoutId = null;
+  this.builtInCommands = {
+    rotate: () => {
+      this.clearTimeout();
+      var rotateFunction = () => {
+        this.angle = (this.angle + 45) % 360;
+        this.emit("command", "roll", [0, this.angle]);
+        this.timeoutId = setTimeout(rotateFunction, 500);
+      };
+      rotateFunction();
+    },
+    stop: () => {
+      this.clearTimeout();
+      this.emit("command", "roll", [0, this.angle]);
+    },
+    dash: () => {
+      if (this.dashTimeoutId !== null) {
+        clearTimeout(this.dashTimeoutId);
+      }
+      this.baseSpeed = 50;
+      this.dashTimeoutId = setTimeout(() => {
+        this.baseSpeed = 0;
+      }, 1000);
+    }
+  };
+}
+
+util.inherits(CommandRunner, EventEmitter);
+
+CommandRunner.prototype.setCommands = function(commands) {
+  this.clearTimeout();
+  this.commands = commands;
+  this.loopMethod(0);
+};
+
+CommandRunner.prototype.setBuiltInCommands = function(builtInCommandsName) {
+  if (typeof this.builtInCommands[builtInCommandsName] !== "function") {
+    throw new Error("built-in command : " + builtInCommandsName + " is not valid.");
+  }
+  this.builtInCommands[builtInCommandsName]();
+};
+
+CommandRunner.prototype.clearTimeout = function() {
+  if (this.timeoutId !== null) {
+    clearTimeout(this.timeoutId);
+  }
+};
+
+CommandRunner.prototype.loopMethod = function(index) {
+  if (this.commands.length === 0) {
+    throw new Error("実行しようとしたcommandsは空でした。: " + this.key);
+  }
+  var currentCommand = this.commands[index];
+  var applyArgs = currentCommand.args.slice();
+  if (currentCommand.commandName === "roll") {
+    applyArgs[0] = this.baseSpeed + applyArgs[0];
+    applyArgs[1] = (360 + applyArgs[1] + this.angle) % 360;
+  }
+  this.emit("command", currentCommand.commandName, applyArgs);
+  var nextIndex = index + 1 >= this.commands.length ? 0 : index + 1;
+  this.timeoutId = setTimeout(() => {
+    this.loopMethod(nextIndex);
+  }, currentCommand.time * 1000);
+};
+
+module.exports = CommandRunner;
+
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var argv = require("argv");
 var config = require("./config");
 var VirtualSphero = require("sphero-ws-virtual-plugin");
 var Dashboard = require("./dashboard");
+var CommandRunner = require("./commandRunner");
 
 var opts = [
   { name: "test", type: "boolean" }
@@ -27,13 +28,14 @@ var players = {};
 var commands = {};
 
 spheroWS.events.on("addClient", function(key, client) {
-  clients[key] = client;
+  clients[key] = {
+    client: client,
+    commandRunner: new CommandRunner(key)
+  };
+  players[key] = {
+    hp: 100
+  };
   if (!isTestMode) {
-    players[key] = {
-      hp: 100,
-      angle: 0,
-      baseSpeed: 0
-    }
     var orb = client.linkedOrb.instance;
     orb.detectCollisions();
     orb.on("collision", function() {
@@ -41,55 +43,22 @@ spheroWS.events.on("addClient", function(key, client) {
       client.sendCustomMessage("hp", { hp: players[key].hp });
     });
   }
+
+  clients[key].commandRunner.on("command", function(commandName, args) {
+    if (!client.linkedOrb.hasCommand(commandName)) {
+      throw new Error("command : " + commandName + " is not valid.");
+    }
+    client.linkedOrb.command(commandName, args);
+  });
+
   client.sendCustomMessage("gameState", { gameState: gameState });
   client.sendCustomMessage("availableCommandsCount", { count: availableCommandsCount });
   client.on("arriveCustomMessage", (name, data, mesID) => {
     if (name === "commands") {
       if (typeof data.type === "string" && data.type === "built-in") {
-        // built-in command arrived.
-        if (typeof commands[key] === "undefined") {
-          commands[key] = {};
-        }
-        switch (data.command) {
-          case "rotate":
-            if (typeof commands[key] !== "undefined" && typeof commands[key].timeoutId !== "undefined") {
-              clearTimeout(commands[key].timeoutId);
-            }
-            console.log("rotating..");
-            var rotateFunction = function() {
-              players[key].angle = (players[key].angle + 45) % 360;
-              if (!isTestMode) {
-                client.linkedOrb.instance.ping(function() {
-                  client.linkedOrb.command("roll", [0, players[key].angle]);
-                });
-              }
-              commands[key].timeoutId = setTimeout(rotateFunction, 500);
-            };
-            rotateFunction();
-            break;
-          case "stop":
-            console.log("stop!");
-            if (typeof commands[key] !== "undefined" && typeof commands[key].timeoutId !== "undefined") {
-              clearTimeout(commands[key].timeoutId);
-            }
-            client.linkedOrb.command("roll", [0, players[key].angle]);
-            break;
-          case "dash":
-            console.log("fufuf");
-            players[key].baseSpeed = 50;
-            setTimeout(function() {
-              players[key].baseSpeed = 0;
-            }, 1000);
-            break;
-        }
+        clients[key].commandRunner.setBuiltInCommands(data.command);
       } else {
-        if (typeof commands[key] !== "undefined" && typeof commands[key].timeoutId !== "undefined") {
-          clearTimeout(commands[key].timeoutId);
-        }
-        commands[key] = {
-          commands: data
-        };
-        commandLoop(key, 0);
+        clients[key].commandRunner.setCommands(data);
       }
     }
   });
@@ -100,32 +69,11 @@ spheroWS.events.on("removeClient", function(key) {
     delete clients[key];
   }
 });
+
 dashboard.on("gameState", (gameState) => {
   gameState = gameState;
   Object.keys(clients).forEach(key => {
-    clients[key].sendCustomMessage("gameState", { gameState: gameState });
+    clients[key].client.sendCustomMessage("gameState", { gameState: gameState });
   });
 });
 
-function commandLoop(key, index) {
-  var currentCommandDetails = commands[key];
-  if (typeof currentCommandDetails === "undefined") {
-    throw new Error("実行しようとしたcommandsはundefinedでした。: " + key);
-  }
-  var currentCommand = currentCommandDetails.commands[index];
-  var orb = clients[key].linkedOrb;
-  if (!orb.hasCommand(currentCommand.commandName)) {
-    throw new Error("command " + currentCommand.commandName + " は存在しませんでした。");
-  }
-  console.log("orb." + currentCommand.commandName + "(" + currentCommand.args.join(", ") + ");");
-  if (!isTestMode) {
-    var applyArgs = currentCommand.args.slice();
-    if (currentCommand.commandName === "roll") {
-      applyArgs[0] = players[key].baseSpeed + applyArgs[0];
-      applyArgs[1] = (360 + applyArgs[1] + players[key].angle) % 360;
-    }
-    orb.command(currentCommand.commandName, applyArgs);
-  }
-  var nextIndex = index + 1 >= currentCommandDetails.commands.length ? 0 : index + 1;
-  currentCommandDetails.timeoutId = setTimeout(commandLoop, currentCommand.time * 1000, key, nextIndex);
-}
