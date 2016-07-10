badef3fcf77704488a76090bbb43c2d8c4a93216
shundroid <shundroid@users.noreply.github.com>
2016-07-10 13:15:20 +0900

    built-in commandに対応

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ spheroWS.events.on("command", function(requestKey, command, args) {
 
 var dashboard = new Dashboard(config.dashboardPort);
 
-var players = {};
 var gameState = "inactive";
 var availableCommandsCount = 6;
 
 var clients = {};
+var players = {};
 // key: {commands, timeoutId, playingIndex}
 var commands = {};
 
@@ -30,7 +30,9 @@ spheroWS.events.on("addClient", function(key, client) {
   clients[key] = client;
   if (!isTestMode) {
     players[key] = {
-      hp: 100
+      hp: 100,
+      angle: 0,
+      baseSpeed: 0
     }
     var orb = client.linkedOrb.instance;
     orb.detectCollisions();
@@ -43,13 +45,52 @@ spheroWS.events.on("addClient", function(key, client) {
   client.sendCustomMessage("availableCommandsCount", { count: availableCommandsCount });
   client.on("arriveCustomMessage", (name, data, mesID) => {
     if (name === "commands") {
-      if (typeof commands[key] !== "undefined" && typeof commands[key].timeoutId !== "undefined") {
-        clearTimeout(commands[key].timeoutId);
+      if (typeof data.type === "string" && data.type === "built-in") {
+        // built-in command arrived.
+        if (typeof commands[key] === "undefined") {
+          commands[key] = {};
+        }
+        switch (data.command) {
+          case "rotate":
+            if (typeof commands[key] !== "undefined" && typeof commands[key].timeoutId !== "undefined") {
+              clearTimeout(commands[key].timeoutId);
+            }
+            console.log("rotating..");
+            var rotateFunction = function() {
+              players[key].angle = (players[key].angle + 45) % 360;
+              if (!isTestMode) {
+                client.linkedOrb.instance.ping(function() {
+                  client.linkedOrb.command("roll", [0, players[key].angle]);
+                });
+              }
+              commands[key].timeoutId = setTimeout(rotateFunction, 500);
+            };
+            rotateFunction();
+            break;
+          case "stop":
+            console.log("stop!");
+            if (typeof commands[key] !== "undefined" && typeof commands[key].timeoutId !== "undefined") {
+              clearTimeout(commands[key].timeoutId);
+            }
+            client.linkedOrb.command("roll", [0, players[key].angle]);
+            break;
+          case "dash":
+            console.log("fufuf");
+            players[key].baseSpeed = 50;
+            setTimeout(function() {
+              players[key].baseSpeed = 0;
+            }, 1000);
+            break;
+        }
+      } else {
+        if (typeof commands[key] !== "undefined" && typeof commands[key].timeoutId !== "undefined") {
+          clearTimeout(commands[key].timeoutId);
+        }
+        commands[key] = {
+          commands: data
+        };
+        commandLoop(key, 0);
       }
-      commands[key] = {
-        commands: data
-      };
-      commandLoop(key, 0);
     }
   });
 });
@@ -78,7 +119,12 @@ function commandLoop(key, index) {
   }
   console.log("orb." + currentCommand.commandName + "(" + currentCommand.args.join(", ") + ");");
   if (!isTestMode) {
-    orb.command(currentCommand.commandName, currentCommand.args);
+    var applyArgs = currentCommand.args.slice();
+    if (currentCommand.commandName === "roll") {
+      applyArgs[0] = players[key].baseSpeed + applyArgs[0];
+      applyArgs[1] = (360 + applyArgs[1] + players[key].angle) % 360;
+    }
+    orb.command(currentCommand.commandName, applyArgs);
   }
   var nextIndex = index + 1 >= currentCommandDetails.commands.length ? 0 : index + 1;
   currentCommandDetails.timeoutId = setTimeout(commandLoop, currentCommand.time * 1000, key, nextIndex);
