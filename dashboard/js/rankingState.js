d0d420577493afba1b407ccc351306ee61dd3b24
shundroid <shundroid@users.noreply.github.com>
2016-07-26 09:46:52 +0900

    rankingStateのbuttonの使い勝手を向上

--- dashboard/js/rankingState.js
+++ dashboard/js/rankingState.js
@@ -6,10 +6,14 @@ export default class rankingState {
     this.element.disabled = true;
     this.rankingState = "hide";
     eventPublisher.on("gameState", gameState => {
+      if (this.rankingState === "show" && gameState === "inactive") {
+        eventPublisher.emit("rankingState", "hide");
+      }
       this.element.disabled = gameState === "inactive";
     });
     eventPublisher.on("rankingState", rankingState => {
       this.rankingState = rankingState;
+      this.element.textContent = this.rankingState === "show" ? "Hide Ranking" : "Show Ranking";
     });
     this.element.addEventListener("click", () => {
       eventPublisher.emit("rankingState", this.rankingState === "show" ? "hide" : "show");
