b0e2e31677ba32a1f67c36e0eaf56fd76717a5dd
shundroid <shundroid@users.noreply.github.com>
2016-07-26 10:14:51 +0900

    nameによるrankingに

--- rankingMaker.js
+++ rankingMaker.js
@@ -2,25 +2,25 @@ export default class RankingMaker {
   constructor() {
   }
   make(controllers) {
-    const controllerKeys = Object.keys(controllers);
+    const controllerNames = Object.keys(controllers);
     // indexが順位となっている
-    // [ { hp: 100, key: "xxx" }, { hp: 80, key: "xxx" }, ...]
-    const ranking = controllerKeys.filter(key => {
+    // [ { hp: 100, name: "xxx" }, { hp: 80, name: "xxx" }, ...]
+    const ranking = controllerNames.filter(name => {
       // まず鬼であるものを除外する
-      return !controllers[key].isOni;
+      return !controllers[name].isOni;
     }).sort((a, b) => {
       // HPに基づき、降順にソートする
       return controllers[b].hp - controllers[a].hp;
-    }).map((key, index, array) => {
+    }).map((name, index, array) => {
       // 必要な情報のみを取り出した新しい配列を生成する
       return {
-        key,
-        hp: controllers[key].hp,
-        isTie: index > 0 && controllers[key].hp === controllers[array[index - 1]].hp
+        name,
+        hp: controllers[name].hp,
+        isTie: index > 0 && controllers[name].hp === controllers[array[index - 1]].hp
       };
     });
-    // [key, key, ...]
-    const onis = controllerKeys.filter(key => controllers[key].isOni);
+    // [name, name, ...]
+    const onis = controllerNames.filter(name => controllers[name].isOni);
     return { ranking, onis };
   }
 }
