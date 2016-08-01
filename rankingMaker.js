ff5bfaf8a91b8c3f1374d5cb2d9f8db5cff8bc55
shundroid <shundroid@users.noreply.github.com>
2016-08-01 11:42:37 +0900

    rankingの形式を変更

--- rankingMaker.js
+++ rankingMaker.js
@@ -7,7 +7,7 @@ export default class RankingMaker {
     // [ { hp: 100, name: "xxx" }, { hp: 80, name: "xxx" }, ...]
     const ranking = controllerNames.filter(name => {
       // まず鬼であるものを除外する
-      return !controllers[name].isOni;
+      return controllers[name].linkedOrb !== null && !controllers[name].isOni;
     }).sort((a, b) => {
       // HPに基づき、降順にソートする
       return controllers[b].hp - controllers[a].hp;
@@ -15,7 +15,7 @@ export default class RankingMaker {
       // 必要な情報のみを取り出した新しい配列を生成する
       return {
         name,
-        hp: controllers[name].hp,
+        states: controllers[name].getStates(),
         isTie: index > 0 && controllers[name].hp === controllers[array[index - 1]].hp
       };
     });
