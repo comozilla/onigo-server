760d49e62027e13540ad09564a95e50e9f4f1734
shundroid <shundroidk@gmail.com>
2017-02-05 19:28:38 +0900

    made publisher.js

--- publisher.js
+++ publisher.js
@@ -1,2 +1,16 @@
-import { EventEmitter } from "events";
-export default new EventEmitter();
+const observeFunctions = {};
+
+export function subscribe(subjectName, observeFunction) {
+  if (typeof observeFunctions[subjectName] === "undefined") {
+    observeFunctions[subjectName] = [];
+  }
+  observeFunctions[subjectName].push(observeFunction);
+}
+
+export function publish(author, subjectName, ...data) {
+  if (typeof observeFunctions[subjectName] !== "undefined") {
+    observeFunctions[subjectName].forEach(observeFunction => {
+      observeFunction(author, ...data);
+    });
+  }
+}
