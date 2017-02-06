13a26222f80508cd58ddd627aaa9af1f0fc0a5cc
shundroid <shundroidk@gmail.com>
2017-02-06 19:40:18 +0900

    supported es6 scoreboard

--- scoreboard.js
+++ scoreboard.js
@@ -4,53 +4,40 @@ import controllerModel from "./controllerModel";
 import RankingMaker from "./rankingMaker";
 import eventPublisher from "./publisher";
 
-let scoreboardInstance = null;
+export default class Scoreboard {
+  constructor(port) {
+    this.app = express();
+    this.server = require("http").Server(this.app);
+    this.io = require("socket.io")(this.server);
+    this.io.origins(`localhost:${port}`);
 
-function Scoreboard(port) {
-  if (scoreboardInstance !== null) {
-    return scoreboardInstance;
+    this.app.use(express.static("scoreboard"));
+    this.server.listen(port, () => {
+      console.log(`score is listening on port ${port}`);
+    });
+
+    this.currentRanking = null;
+
+    this.rankingMaker = new RankingMaker();
+
+    this.sockets = [];
+    this.io.on("connection", socket => {
+      console.log("a scoreboard connected.");
+      this.sockets.push(socket);
+      if (this.currentRanking !== null) {
+        socket.emit("data", this.currentRanking);
+      }
+    });
+
+    eventPublisher.subscribe("updatedHp", this.updateRanking.bind(this));
+    eventPublisher.subscribe("updatedLink", this.updateRanking.bind(this));
+    eventPublisher.subscribe("updatedColor", this.updateRanking.bind(this));
   }
-  scoreboardInstance = this;
 
-  this.app = express();
-  this.server = require("http").Server(this.app);
-  this.io = require("socket.io")(this.server);
-  this.io.origins(`localhost:${port}`);
-
-  this.app.use(express.static("scoreboard"));
-  this.server.listen(port, () => {
-    console.log(`score is listening on port ${port}`);
-  });
-
-  this.currentRanking = null;
-
-  this.rankingMaker = new RankingMaker();
-
-  this.sockets = [];
-  this.io.on("connection", socket => {
-    console.log("a scoreboard connected.");
-    this.sockets.push(socket);
-    if (this.currentRanking !== null) {
+  updateRanking() {
+    this.currentRanking = this.rankingMaker.make(controllerModel.controllers);
+    this.sockets.forEach(socket => {
       socket.emit("data", this.currentRanking);
-    }
-  });
-
-  eventPublisher.on("updatedHp", () => {
-    this.updateRanking();
-  });
-  eventPublisher.on("updateLink", () => {
-    this.updateRanking();
-  });
-  eventPublisher.on("color", () => {
-    this.updateRanking();
-  });
+    });
+  }
 }
-
-Scoreboard.prototype.updateRanking = function() {
-  this.currentRanking = this.rankingMaker.make(controllerModel.controllers);
-  this.sockets.forEach(socket => {
-    socket.emit("data", this.currentRanking);
-  });
-};
-
-module.exports = Scoreboard;
